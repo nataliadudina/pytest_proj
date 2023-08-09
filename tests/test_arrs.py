f2297ce30352d1427af821b8dfd000b6e4b7468d
NataliaDudina <dudina.natalja@rambler.ru>
2023-08-09 23:07:23 +0300

    add my_slice tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -12,3 +12,6 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 1, 3) == []
+    assert arrs.my_slice([1, 2, 3, 4], -2) == [3, 4]
+    assert arrs.my_slice([1, 2, 3, 4], -6) == [1, 2, 3, 4]
